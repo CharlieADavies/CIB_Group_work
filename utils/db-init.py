b2545dae733232233ad299c9b0bbe92dfeaa9a1f
Charlie <charlieadavies@gmail.com>
2019-04-29 14:17:07 +0100

    added functions to connect to the database using credentials from the secrets.json file

--- utils/db-init.py
+++ utils/db-init.py
@@ -1,3 +1,31 @@
 """
 gets the object with connection details for the database - requires serets.json
 """
+import json
+
+import mysql.connector
+
+
+def load_credentials(f):
+    with open(f, "r+") as f:
+        creds = json.load(f)
+    return creds
+
+
+def connect(user, database, password, host):
+    return mysql.connector.connect(user=user, database=database, password=password, host=host)
+
+
+def load_db(file_path="../secrets.json"):
+    """
+    :return: mysql.connector.connect() whatever that returns
+    :param file_path: the file path of the database secrets file
+    """
+    creds = load_credentials(file_path)
+    return connect(user=creds['user'],
+                   database=creds['database'],
+                   password=creds['password'],
+                   host=creds['host'])
+
+
+print(load_db())

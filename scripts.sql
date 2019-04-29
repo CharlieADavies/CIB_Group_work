803201fadb14f506310f9df9cd1d27b0c3c5078a
Charlie <charlieadavies@gmail.com>
2019-04-29 13:05:26 +0100

    user table create

---
+++ scripts.sql
@@ -0,0 +1,13 @@
+create table user
+(
+    username    varchar(50) primary key,
+    password    varchar(50),
+    role        varchar(50) default "EMPLOYEE",
+    phone_no    varchar(50),
+    first_name  varchar(50),
+    last_name   varchar(50),
+    employee_no varchar(50),
+    address     varchar(250),
+    is_blue_badge  TINYINT(1)         default 0
+);
+

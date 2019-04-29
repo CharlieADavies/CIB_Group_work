67c5ff583117b97181a74a692dfd93a39f018284
Charlie <charlieadavies@gmail.com>
2019-04-29 15:19:42 +0100

    added outline for login view

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+app.secret_key = "secret-key"
+
+@app.route("/login")
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    # TODO add the database function that verifies stuff here
 
 
 @app.route('/')

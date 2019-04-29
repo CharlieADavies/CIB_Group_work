71eafb751da66c4f90744449bcbefb6291242793
DenisD <denisdstudent@gmail.com>
2019-04-29 14:31:18 +0100

    Mainframe - So far, only login page. All gui on here. Different classes for different pages.

---
+++ tkinter-code/MainFrame.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+from tkinter import font as tkfont
+from tkinter import ttk
+
+
+class Application(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        self.title_font = tkfont.Font(family='Helvetica', size=20, weight="bold")
+        self.label_font = tkfont.Font(family='Helvetica', size=15)
+
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        window_manager = tk.Frame(self)
+        window_manager.pack(side="top", fill="both", expand=True)
+        window_manager.grid_rowconfigure(0, weight=1)
+        window_manager.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for pages in (LoginScreen, ):
+            page_name = pages.__name__
+            frame = pages(parent=window_manager, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.switch_frame("LoginScreen")
+        self.title("B&Q Parking")
+
+    def switch_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+class LoginScreen(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        title = tk.Label(self, text="Please Login", font=controller.title_font).grid(column=2, row=1)
+
+        username_label = tk.Label(self, text="Username: ", font=controller.label_font).grid(column=1, row=2)
+        username_entry = tk.Entry(self).grid(column=2, row=2)
+
+        password_label = tk.Label(self, text="Password: ", font=controller.label_font).grid(column=1, row=3)
+        password_entry = tk.Entry(self).grid(column=2, row=3)
+
+        login_button = tk.Button(self, text="Login", font=controller.label_font).grid(column=2, row=4)
+
+
+if __name__ == "__main__":
+    app = Application()
+    app.mainloop()
+

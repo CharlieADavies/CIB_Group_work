e14fca128c4e615d71aa55c9581ae305502da8e6
Charlie <charlieadavies@gmail.com>
2019-04-30 09:23:24 +0100

    added login required decorator

--- flask_code/decorators.py
+++ flask_code/decorators.py
@@ -5,7 +5,8 @@ from flask import g, request, redirect, url_for, session
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if 'username' not in session.keys() is None:
+        if 'username' not in session is None:
             return redirect(url_for('login', next=request.url))
         return f(*args, **kwargs)
+
     return decorated_function

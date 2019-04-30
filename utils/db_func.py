a3df30ba26d8b2e5e817741b2d26abe430940115
andrescorjim <andrescorjim@users.noreply.github.com>
2019-04-30 15:29:43 +0100

    Update db_func.py

--- utils/db_func.py
+++ utils/db_func.py
@@ -11,14 +11,14 @@ connection = mysql.connector.connect(host='you-fail.net',
                                      password='H4p9Hcf3scCytzr')
 
 
-def vehicles_insert_query(userID, electricVehicle, registrationVehicle, makeVehicle, blueBadge):
+def vehicles_insert_query(userID, electricVehicle, registrationVehicle, makeVehicle):
     try:
         creds = utils.db_init.load_credentials("./secrets.json")
         connect_sql = utils.db_init.connect(creds['user'], creds['database'], creds['password'], creds['host'])
         cursor = connection.cursor(prepared=True)
         sql_insert_query = """ INSERT INTO `vehicles`
-                          (`username`, `electric_vehicle`, `vehicle_registration`, `vehicle_make`,`is_blue_badge`) VALUES (%s,%s,%s,%s,%s)"""
-        insert_tuple = (userID, electricVehicle, registrationVehicle, makeVehicle, blueBadge)
+                          (`username`, `electric_vehicle`, `vehicle_registration`, `vehicle_make`) VALUES (%s,%s,%s,%s,%s)"""
+        insert_tuple = (userID, electricVehicle, registrationVehicle, makeVehicle)
         result  = cursor.execute(sql_insert_query, insert_tuple)
         connection.commit()
         print ("Record inserted successfully into vehicles table")
@@ -26,4 +26,4 @@ def vehicles_insert_query(userID, electricVehicle, registrationVehicle, makeVehi
         #connection.rollback()
         print("Failed to insert into vehicles table {}".format(error))
 
-vehicles_insert_query("andrescordovajim@hotmail.com", 0, "CE55 BAD", "Peugeot",0)
+vehicles_insert_query("andrescordovajim@hotmail.com", 0, "CE55 BAD", "Peugeot")

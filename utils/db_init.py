f82c2bcb88e024fcdb3ea93b8b0e0daf4ec9060a
CharlieADavies <charlieadavies@gmail.com>
2019-04-30 23:54:30 +0100

    removed non-functional db_init

--- utils/db_init.py
+++ utils/db_init.py
@@ -6,7 +6,7 @@ import json
 import mysql.connector
 
 
-def load_credentials(f):
+def load_credentials(f="../secrets.json"):
     with open(f, "r+") as f:
         creds = json.load(f)
     return creds
@@ -14,18 +14,4 @@ def load_credentials(f):
 
 def connect(user, database, password, host):
     return mysql.connector.connect(user=user, database=database, password=password, host=host)
-
-
-def load_db(file_path="../secrets.json"):
-    """
-    :return: mysql.connector.connect() whatever that returns
-    :param file_path: the file path of the database secrets file
-    """
-    creds = load_credentials(file_path)
-    return connect(user=creds['user'],
-                   database=creds['database'],
-                   password=creds['password'],
-                   host=creds['host']).cursor()
-
-
 

e91bc915171b3b03d0a6f31984ec858675f40af8
Charlie <charlieadavies@gmail.com>
2019-04-30 15:24:39 +0100

    Added function to get users who have a booking in the next 3 days

--- utils/db_func.py
+++ utils/db_func.py
@@ -3,18 +3,18 @@ database functionality common to both flask and tkinter
 """
 import mysql
 
-"""
-import these
+
+
 import mysql.connector
 from mysql.connector import Error
 from mysql.connector import errorcode
-"""
+
 
 def vehicles_insert_query(userID, electricVehicle, registrationVehicle, makeVehicle, blueBadge):
     try:
         connection = mysql.connector.connect(host='you-fail.net',
                              database='youfailn_cib',
-                             user='youfailn_cb',
+                             user='youfailn_cib',
                              password='H4p9Hcf3scCytzr')
         cursor = connection.cursor(prepared=True)
         sql_insert_query = """ INSERT INTO `vehicles`
@@ -24,7 +24,7 @@ def vehicles_insert_query(userID, electricVehicle, registrationVehicle, makeVehi
         connection.commit()
         print ("Record inserted successfully into vehicles table")
     except mysql.connector.Error as error :
-        connection.rollback()
+        #connection.rollback()
         print("Failed to insert into vehicles table {}".format(error))
 
 vehicles_insert_query("andrescordovajim@hotmail.com", 0, "CE55 BAD", "Peugeot",0)

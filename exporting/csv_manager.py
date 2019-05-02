7ba34cb04f422b3daf396b0ad2287f2ddc29cecd
SDaley0 <40636666+SDaley0@users.noreply.github.com>
2019-05-02 09:09:45 +0100

    CSV export from database

--- exporting/csv_manager.py
+++ exporting/csv_manager.py
@@ -1,14 +1,7 @@
 import utils.db_init
-
-
-def get_as_csv(rows, which_table):
-    creds = utils.db_init.load_credentials()
-    connection = utils.db_init.connect(creds['user'], creds['database'], creds['password'], creds['host'])
-    cursor = connection.cursor()
-    sql_query = "SELECT * from " + which_table
-    cursor.execute(sql_query)
-    for i in range(0, rows):
-        print(cursor.fetchone())
+import pandas
+import xlsxwriter
+import dataframe
 
 
 def find_rows(which_table):
@@ -17,24 +10,22 @@ def find_rows(which_table):
     cursor = connection.cursor()
     sql_rows = "SELECT COUNT(*) FROM " + which_table
     cursor.execute(sql_rows)
-    return cursor.fetchone()[0]
+    get_as_csv(cursor.fetchone()[0], which_table)
 
 
-print("Table: Users")
+def get_as_csv(rows, which_table):
+    creds = utils.db_init.load_credentials()
+    connection = utils.db_init.connect(creds['user'], creds['database'], creds['password'], creds['host'])
+    cursor = connection.cursor()
+    sql_query = "SELECT * from " + which_table
+    cursor.execute(sql_query)
+    print("Table: " + which_table)
+    for i in range(0, rows):
+        print(cursor.fetchone())
+    print("")
+
+
 find_rows("users")
-get_as_csv(find_rows("users"), "users")
-print("")
-
-print("Table: Vehicles")
 find_rows("vehicles")
-get_as_csv(find_rows("vehicles"), "vehicles")
-print("")
-
-print("Table: Bookings")
 find_rows("bookings")
-get_as_csv(find_rows("bookings"), "bookings")
-print("")
-
-print("Table: Badge Colours")
 find_rows("badge_colours")
-get_as_csv(find_rows("badge_colours"), "badge_colours")
